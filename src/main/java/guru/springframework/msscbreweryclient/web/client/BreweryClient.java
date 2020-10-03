ae6676016be93c85dd01f8a994a9d53f9e1fdf01
JuanCalMun <juancalmun@gmail.com>
2020-10-03 21:26:32 +0200

    Changed way to check application.properties value

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -2,7 +2,7 @@ package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
 import guru.springframework.msscbreweryclient.web.model.CustomerDto;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -11,16 +11,17 @@ import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
 @Component
-@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
 
     public final static String BEER_PATH_V1 = "/api/v1/beer";
     public final static String CUSTOMER_PATH_V1 = "/api/v1/customer";
-    private String apihost;
+    private final String apihost;
 
     private RestTemplate restTemplate;
 
-    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+    public BreweryClient(@Value("${sfg.brewery.apihost}") String apihost,
+                         RestTemplateBuilder restTemplateBuilder) {
+        this.apihost = apihost;
         restTemplate = restTemplateBuilder.build();
     }
 
@@ -66,7 +67,4 @@ public class BreweryClient {
         restTemplate.delete(apihost + CUSTOMER_PATH_V1 + "/" + customerId.toString());
     }
 
-    public void setApihost(String apihost) {
-        this.apihost = apihost;
-    }
 }
